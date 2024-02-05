672d4fd716b1eba6380e7d53a52111174c569590
Voltaire-000 <87976056+Voltare-000@users.noreply.github.com>
2024-02-05 21:23:23 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Console.Write("Enter a string: ");
+        //string input = Console.ReadLine();
+        string input = "hello";
+        string hash = ComputeSha256Hash(input);
+        Console.WriteLine($"The SHA256 hash of '{input}' is: {hash}");
+    }
+
+    static string ComputeSha256Hash(string rawData)
+    {
+        using (SHA256 sha256Hash = SHA256.Create())
+        {
+            byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                builder.Append(bytes[i].ToString("x2"));
+            }
+            return builder.ToString();
+        }
+    }
+}
+
